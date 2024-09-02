dedc0ab4c0ecea6f1a52e333ec34bf3f1558a285
vicont22 <github@selectfix.com>
2024-09-02 17:27:17 -0400

    added grapgicsPoint when selectin a city

--- vicgis1api/script.js
+++ vicgis1api/script.js
@@ -6,9 +6,11 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
   "esri/layers/GraphicsLayer",
   "esri/layers/FeatureLayer",
   "esri/views/layers/LayerView",
-  "esri/rest/support/Query"], 
+  "esri/rest/support/Query",
+  "esri/symbols/SimpleFillSymbol",
+  "esri/symbols/SimpleLineSymbol"], 
   function(esriConfig, Map, MapView, WebMap, Legend, LayerList, Point, Graphic, 
-    GraphicsLayer, FeatureLayer, LayerView, Query) {
+    GraphicsLayer, FeatureLayer, LayerView, Query, SimpleFillSymbol, SimpleLineSymbol) {
 
     // esriConfig.apiKeyAA = "..PTxy8BH1VEsoebNVZXo8HurA00kGrEUM88Me3K5X12dKEX4TMcmo0W5Wax2c5rnRwj-D4jZce2c7SyOloOm4jt6233p_p2PIiu95_P_u0z4qgefMAUj5pxwcOumkpAUl1GWo797V1TSq6liHwqLGbBz7NJhkfayFFu_lWOAgnerk9kzxdhqv8g9xUpRCAl8Gu1ZHRzkO6ZNKnoZORzJDIwN-Pg3_Cwh7ZIYEpsKzAoQSyz0ll4WcJBscktusFy0dPPAT1_IjObyirT";
     // const map = new Map({
@@ -20,7 +22,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
     // webmap id: 4d60688e31844dcdbc81bd6487f293ec  drug sites use require Vic login as share is not public
     // webmap id: cf35269c59cd45cc9bfae3480c0a64d7 webmap of transcanadian trails from Content/LivingAtlas so is public
     // webmap id: 3b484bf1b7cf4e33beaca992bdfc3c2f  topographic map public also from LivingAtlas
-  // basemap 'hybrid' can be used  without apiKey
+    // basemap 'hybrid' can be used  without apiKey
 
     // const map = new Map({
     // basemap: "hybrid" // basemap styles service
@@ -41,26 +43,26 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
     });
 
   
-  // view.on("click", function(event){
-  //   view.popup = null; //supress existing popup info
-  //   let latitude =event.mapPoint.latitude;
-  //   let longitude =event.mapPoint.longitude;
-  //   // console.log(event);
-  //   // console.log(latitude, longitude);
-  //   document.getElementById("yCoordinate").textContent = latitude;
-  //   document.getElementById("xCoordinate").textContent = longitude
-  // });
-  //     //create and add Legend vidget to the view
-  //     const legendWidget = new Legend({
-  //       view: view
-  //     })
-  //     view.ui.add(legendWidget, "bottom-left");
+    // view.on("click", function(event){
+    //   view.popup = null; //supress existing popup info
+    //   let latitude =event.mapPoint.latitude;
+    //   let longitude =event.mapPoint.longitude;
+    //   // console.log(event);
+    //   // console.log(latitude, longitude);
+    //   document.getElementById("yCoordinate").textContent = latitude;
+    //   document.getElementById("xCoordinate").textContent = longitude
+    // });
+    //     //create and add Legend vidget to the view
+    //     const legendWidget = new Legend({
+    //       view: view
+    //     })
+    //     view.ui.add(legendWidget, "bottom-left");
 
-  //     // add LayerList widget
-  //     let layerList = new LayerList({
-  //       view: view
-  //     })
-  //     view.ui.add(layerList, "top-left"); //already shows up ok, no change to html
+    //     // add LayerList widget
+    //     let layerList = new LayerList({
+    //       view: view
+    //     })
+    //     view.ui.add(layerList, "top-left"); //already shows up ok, no change to html
    
   //     // now we extract layer name from webmap layers property to use in html dropdown
   //     view.when().then(() => {
@@ -98,67 +100,82 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
         // url: 'https://services2.arcgis.com/rDdKyyk7uludsLpI/arcgis/rest/services/Hazards_Public/FeatureServer'
       });
 
-view.when().then(() => {
+  view.when().then(() => {
 
-  layer.queryFeatureCount().then(function (numFeatures) {
-    //total count to the console
-    document.getElementById("layerResult").textContent = numFeatures;
-    console.log(numFeatures);
-  })
-    // below does not show value?
-  view.whenLayerView(layer).then(function(LayerView) {
-    //do simething with LayerView, by ex, zoom
-    LayerView.watch("updating", function (value){
-      if (!value){
-        LayerView.queryFeatureCount().then(function(numFeaturess){
-          //total count from layerView
-          document.getElementById("layerViewResult").textContent = numFeaturess;
-          console.log(numFeaturess);
-        });
-      }
+    layer.queryFeatureCount().then(function (numFeatures) {
+      //total count to the console
+      document.getElementById("layerResult").textContent = numFeatures;
+      console.log(numFeatures);
+    })
+      // below does not show value?
+    view.whenLayerView(layer).then(function(LayerView) {
+      //do simething with LayerView, by ex, zoom
+      LayerView.watch("updating", function (value){
+        if (!value){
+          LayerView.queryFeatureCount().then(function(numFeaturess){
+            //total count from layerView
+            document.getElementById("layerViewResult").textContent = numFeaturess;
+            console.log(numFeaturess);
+          });
+        }
+      });
     });
   });
-});
-// qyert Feature result using parameter from our Input 
-view.ui.add(document.getElementById("queryFeatures"), "top-left");
-document.getElementById("queryBtn").addEventListener("click", queryFeatureLayer);
+  // query Feature result using parameter from our Input 
+  view.ui.add(document.getElementById("queryFeatures"), "top-left");
+  document.getElementById("queryBtn").addEventListener("click", queryFeatureLayer);
 
-function queryFeatureLayer(){
-  let featureName = document.getElementById("searchInput").value;
-  // alert(featureName);
-  // console.log(featureName);
-  // //create query for the layer
-  let query = layer.createQuery();
-  // //define the parameters for he query
-  // query.where = "1=1";
+  function queryFeatureLayer(){
+    let featureName = document.getElementById("searchInput").value;
+    // alert(featureName);
+    // console.log(featureName);
+    // //create query for the layer
+      let query = layer.createQuery();
+    // //define the parameters for he query
+    // query.where = "1=1";
 
-  // // works when enter site name 'Four Cast' - responds with city name
-  // query.where = `site = '${featureName}'`;
-  // query.outFields =["*"];
-  // query.returnGeometry = true;
-  // //execute the query
-  // layer.queryFeatures(query).then((result) => {
-  //   console.log(result);
-  //   result.features.map((feature) => {
-  //     let cityName = feature.attributes["city"];
-  //     document.getElementById("queryResultInfo").textContent = `${featureName} is in ${cityName} city`
-  //   })
+    // // works when enter site name 'Four Cast' - responds with city name
+    // query.where = `site = '${featureName}'`;
+    // query.outFields =["*"];
+    // query.returnGeometry = true;
+    // //execute the query
+    // layer.queryFeatures(query).then((result) => {
+    //   console.log(result);
+    //   result.features.map((feature) => {
+    //     let cityName = feature.attributes["city"];
+    //     document.getElementById("queryResultInfo").textContent = `${featureName} is in ${cityName} city`
+    //   })
 
-  // works when enter sity name 'Guelf' - responds with site center name
-  query.where = `city = '${featureName}'`;
-  query.outFields =["*"];
-  query.returnGeometry = true;
-  //execute the query
-  layer.queryFeatures(query).then((result) => {
-    console.log(result);
-    result.features.map((feature) => {
-      let siteName = feature.attributes["site"];
-      document.getElementById("queryResultInfo").textContent = `${featureName} has site named ${siteName}`
-    })
+    // works when enter sity name 'Guelf' - responds with site center name
+    query.where = `city = '${featureName}'`;
+    query.outFields =["*"];
+    query.returnGeometry = true;
+    //execute the query
+    layer.queryFeatures(query).then((result) => {
+      console.log(result);
+      result.features.map((feature) => {
+        let siteName = feature.attributes["site"];
+        document.getElementById("queryResultInfo").textContent = `${featureName} has site named ${siteName}`;
 
-  })
+        let latPoint = feature.attributes["lat"];
+        let longPoint = feature.attributes["long"];
+        let newPoint = new Point({
+          longitude: `${longPoint}`,
+          latitude: `${latPoint}`,
+        });
+         //create a point graphic
+         const pointGraphic = new Graphic({
+          geometry: newPoint,
+         }) 
+         let newGraphicLayer = new GraphicsLayer({});
+         view.graphics.add(pointGraphic);
+         view.map.add(newGraphicLayer);
 
-}
+
+      });
+    });
+  }
+
 
 
 
