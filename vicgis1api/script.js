5c7965438aec5744ba809e548e35d789ae463018
vicont22 <github@selectfix.com>
2024-09-03 11:54:45 -0400

    added id and service for widfire

--- vicgis1api/script.js
+++ vicgis1api/script.js
@@ -13,6 +13,10 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
     // webmap id: 4d60688e31844dcdbc81bd6487f293ec  drug sites use require Vic login as share is not public
     // webmap id: cf35269c59cd45cc9bfae3480c0a64d7 webmap of transcanadian trails from Content/LivingAtlas so is public
     // webmap id: 3b484bf1b7cf4e33beaca992bdfc3c2f  topographic map public also from LivingAtlas
+    // webmap id: 230bcf5780b34f409686a0ca2c084804   wildfires Canada from Living Atlas
+    // id: 0fa855d41cfd488c830b0a4942f710a8  same wildfire as public atals
+    // same wildfire as service
+    // https://services.arcgis.com/wjcPoefzjpzCgffS/arcgis/rest/services/Active_Wildfire_Perimeters_in_Canada_View/FeatureServer
   // basemap 'hybrid' can be used  without apiKey
 
     // const map = new Map({
@@ -22,7 +26,7 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
     //Instead of webmap we use WebMap class
     const webmap = new WebMap({
       portalItem: { // autocasts as new PortalItem()
-        id: "cf35269c59cd45cc9bfae3480c0a64d7"
+        id: "0fa855d41cfd488c830b0a4942f710a8"
       }
     });
 
@@ -34,15 +38,15 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
     });
 
   
-  view.on("click", function(event){
-    view.popup = null; //supress existing popup info
-    let latitude =event.mapPoint.latitude;
-    let longitude =event.mapPoint.longitude;
-    // console.log(event);
-    // console.log(latitude, longitude);
-    document.getElementById("yCoordinate").textContent = latitude;
-    document.getElementById("xCoordinate").textContent = longitude
-  });
+  // view.on("click", function(event){
+  //   view.popup = null; //supress existing popup info
+  //   let latitude =event.mapPoint.latitude;
+  //   let longitude =event.mapPoint.longitude;
+  //   // console.log(event);
+  //   // console.log(latitude, longitude);
+  //   document.getElementById("yCoordinate").textContent = latitude;
+  //   document.getElementById("xCoordinate").textContent = longitude
+  // });
       //create and add Legend vidget to the view
       const legendWidget = new Legend({
         view: view
@@ -50,24 +54,24 @@ require(["esri/config", "esri/Map", "esri/views/MapView", "esri/WebMap",
       view.ui.add(legendWidget, "bottom-left");
 
       // add LayerList widget
-      let layerList = new LayerList({
-        view: view
-      })
-      view.ui.add(layerList, "top-left"); //already shows up ok, no change to html
+      // let layerList = new LayerList({
+      //   view: view
+      // })
+      // view.ui.add(layerList, "top-left"); //already shows up ok, no change to html
    
       // now we extract layer name from webmap layers property to use in html dropdown
-      view.when().then(() => {
-        webmap.layers.map((Layer)=>{
-          console.log(Layer.title);
-          let option = document.createElement("option");
-          option.textContent = Layer.title;
-          let select = document.getElementById("layerName")
-          select.appendChild(option);
-        })
-        let lyrList = document.getElementById("lyrList");
-        view.ui.add(lyrList, "bottom-right");
+      // view.when().then(() => {
+      //   webmap.layers.map((Layer)=>{
+      //     console.log(Layer.title);
+      //     let option = document.createElement("option");
+      //     option.textContent = Layer.title;
+      //     let select = document.getElementById("layerName")
+      //     select.appendChild(option);
+      //   })
+      //   let lyrList = document.getElementById("lyrList");
+      //   view.ui.add(lyrList, "bottom-right");
 
-      })
+      // })
 
 
 
